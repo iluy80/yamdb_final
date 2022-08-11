2af7dae0534bcf7a3d338bee3430a8eb03c56387
Илья Гаврилов <iluy80@yandex.ru>
2022-08-12 02:01:01 +0300

    Добавил проврку Flake8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -20,6 +20,6 @@ class IsAuthenticatedOrReadOnly(BasePermission):
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return(request.method in SAFE_METHODS
-               or request.user.is_authenticated
-               and request.user.is_admin())
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated
+                and request.user.is_admin())
