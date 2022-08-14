a1bf1bc631dd974193c82d9c6c6afdf40209fd95
Илья Гаврилов <iluy80@yandex.ru>
2022-08-14 13:06:32 +0300

    Работа над ошибками7

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['130.193.55.38']
 
 
 # Application definition
